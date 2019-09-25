openwrt/luci

luci-mod-network: Add mesh_rssi_threshold and encryption
* make mesh_rssi_threshold configurable
* add SAE encryption support

Signed-off-by: Enrique Giraldo <enrique.giraldo@galgus.net>

modules/luci-mod-network/luasrc/model/cbi/admin_network/wifi.lua
@@ -376,6 +376,14 @@ meshfwd.rmempty = false
 meshfwd.default = "1"
 meshfwd:depends({mode="mesh"})
 
+mesh_rssi_th = s:taboption("advanced", Value, "mesh_rssi_threshold",
+       translate("RSSI threshold for joining"),
+       translate("0 = not using RSSI threshold, 1 = do not change driver default"))
+mesh_rssi_th.rmempty = false
+mesh_rssi_th.default = "0"
+mesh_rssi_th.datatype = "range(-255,1)"
+mesh_rssi_th:depends({mode="mesh"})
+
 ssid = s:taboption("general", Value, "ssid", translate("<abbr title=\"Extended Service Set Identifier\">ESSID</abbr>"))
 ssid.datatype = "maxlength(32)"
 ssid:depends({mode="ap"})
@@ -669,6 +677,7 @@ if hwtype == "mac80211" or hwtype == "prism2" then
 		encr:value("psk", "WPA-PSK", {mode="ap"}, {mode="sta"}, {mode="ap-wds"}, {mode="sta-wds"}, {mode="adhoc"})
 		encr:value("psk2", "WPA2-PSK", {mode="ap"}, {mode="sta"}, {mode="ap-wds"}, {mode="sta-wds"}, {mode="adhoc"})
 		encr:value("psk-mixed", "WPA-PSK/WPA2-PSK Mixed Mode", {mode="ap"}, {mode="sta"}, {mode="ap-wds"}, {mode="sta-wds"}, {mode="adhoc"})
+		encr:value("authsae", "SAE", {mode="mesh"})
 		if has_ap_eap and has_sta_eap then
 			encr:value("wpa", "WPA-EAP", {mode="ap"}, {mode="sta"}, {mode="ap-wds"}, {mode="sta-wds"})
 			encr:value("wpa2", "WPA2-EAP", {mode="ap"}, {mode="sta"}, {mode="ap-wds"}, {mode="sta-wds"})
@@ -689,6 +698,7 @@ if hwtype == "mac80211" or hwtype == "prism2" then
 		encr:value("psk", "WPA-PSK", {mode="sta"}, {mode="sta-wds"}, {mode="adhoc"})
 		encr:value("psk2", "WPA2-PSK", {mode="sta"}, {mode="sta-wds"}, {mode="adhoc"})
 		encr:value("psk-mixed", "WPA-PSK/WPA2-PSK Mixed Mode", {mode="sta"}, {mode="sta-wds"}, {mode="adhoc"})
+		encr:value("authsae", "SAE", {mode="mesh"})
 		if has_sta_eap then
 			encr:value("wpa", "WPA-EAP", {mode="sta"}, {mode="sta-wds"})
 			encr:value("wpa2", "WPA2-EAP", {mode="sta"}, {mode="sta-wds"})
@@ -839,6 +849,21 @@ for slot=1,4 do
 		end
 		return Value.write(self, section, value)
 	end
+end
+
+authsae = s:taboption("encryption", Value, "_sae_key", translate("Key"))
+authsae:depends("encryption", "authsae")
+authsae.rmempty = true
+authsae.datatype = "wpakey"
+authsae.password = true
+
+authsae.cfgvalue = function(self, section, value)
+	local key = m.uci:get("wireless", section, "key")
+	return key
+end
+
+authsae.write = function(self, section, value)
+	self.map.uci:set("wireless", section, "key", value)
 end
 
 if hwtype == "mac80211" or hwtype == "prism2" then

mips171/telcos_luci

modules/luci-mod-network/luasrc/model/cbi/admin_network/wifi.lua
@@ -376,6 +376,14 @@ meshfwd.rmempty = false
 meshfwd.default = "1"
 meshfwd:depends({mode="mesh"})
 
+mesh_rssi_th = s:taboption("advanced", Value, "mesh_rssi_threshold",
+       translate("RSSI threshold for joining"),
+       translate("0 = not using RSSI threshold, 1 = do not change driver default"))
+mesh_rssi_th.rmempty = false
+mesh_rssi_th.default = "0"
+mesh_rssi_th.datatype = "range(-255,1)"
+mesh_rssi_th:depends({mode="mesh"})
+
 ssid = s:taboption("general", Value, "ssid", translate("<abbr title=\"Extended Service Set Identifier\">ESSID</abbr>"))
 ssid.datatype = "maxlength(32)"
 ssid:depends({mode="ap"})
@@ -669,6 +677,7 @@ if hwtype == "mac80211" or hwtype == "prism2" then
 		encr:value("psk", "WPA-PSK", {mode="ap"}, {mode="sta"}, {mode="ap-wds"}, {mode="sta-wds"}, {mode="adhoc"})
 		encr:value("psk2", "WPA2-PSK", {mode="ap"}, {mode="sta"}, {mode="ap-wds"}, {mode="sta-wds"}, {mode="adhoc"})
 		encr:value("psk-mixed", "WPA-PSK/WPA2-PSK Mixed Mode", {mode="ap"}, {mode="sta"}, {mode="ap-wds"}, {mode="sta-wds"}, {mode="adhoc"})
+		encr:value("sae", "SAE", {mode="mesh"})
 		if has_ap_eap and has_sta_eap then
 			encr:value("wpa", "WPA-EAP", {mode="ap"}, {mode="sta"}, {mode="ap-wds"}, {mode="sta-wds"})
 			encr:value("wpa2", "WPA2-EAP", {mode="ap"}, {mode="sta"}, {mode="ap-wds"}, {mode="sta-wds"})
@@ -689,6 +698,7 @@ if hwtype == "mac80211" or hwtype == "prism2" then
 		encr:value("psk", "WPA-PSK", {mode="sta"}, {mode="sta-wds"}, {mode="adhoc"})
 		encr:value("psk2", "WPA2-PSK", {mode="sta"}, {mode="sta-wds"}, {mode="adhoc"})
 		encr:value("psk-mixed", "WPA-PSK/WPA2-PSK Mixed Mode", {mode="sta"}, {mode="sta-wds"}, {mode="adhoc"})
+		encr:value("sae", "SAE", {mode="mesh"})
 		if has_sta_eap then
 			encr:value("wpa", "WPA-EAP", {mode="sta"}, {mode="sta-wds"})
 			encr:value("wpa2", "WPA2-EAP", {mode="sta"}, {mode="sta-wds"})
@@ -839,6 +849,21 @@ for slot=1,4 do
 		end
 		return Value.write(self, section, value)
 	end
+end
+
+saekey = s:taboption("encryption", Value, "_sae_key", translate("Key"))
+saekey:depends("encryption", "sae")
+saekey.rmempty = true
+saekey.datatype = "wpakey"
+saekey.password = true
+
+saekey.cfgvalue = function(self, section, value)
+	local key = m.uci:get("wireless", section, "key")
+	return key
+end
+
+saekey.write = function(self, section, value)
+	self.map.uci:set("wireless", section, "key", value)
 end
 
 if hwtype == "mac80211" or hwtype == "prism2" then