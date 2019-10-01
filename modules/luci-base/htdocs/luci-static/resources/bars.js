964e7fc22015e6bfc487c3921329a0352cdebb9e
Nicholas Smith <nicholas.smith@telcoantennas.com.au>
2019-10-01 10:03:32 +1000

    add bars.js

---
+++ modules/luci-base/htdocs/luci-static/resources/bars.js
@@ -0,0 +1,30 @@
+/*
+	Bars for LuCI
+	
+	Pass this function a value in seconds and it will make a progress bar for you.
+	Copyright 2018 Nicholas Smith <mips171@icloud.com>
+	Licensed under the GNU GPLv2.0
+	
+*/
+
+function createProgressbar(id, duration, callback) {
+  /* We select the div that we want to turn into a progressbar */
+  var progressbar = document.getElementById(id);
+  progressbar.className = 'progressbar';
+
+  /* We create the div that changes width to show progress */
+  var progressbarinner = document.createElement('div');
+  progressbarinner.className = 'inner';
+
+  /* Now we set the animation parameters */
+  progressbarinner.style.animationDuration = duration;
+
+  /* Eventually couple a callback */
+  if (typeof(callback) === 'function') {
+    progressbarinner.addEventListener('animationend', callback);
+  }
+
+  progressbar.appendChild(progressbarinner);
+
+  progressbarinner.style.animationPlayState = 'running';
+}
