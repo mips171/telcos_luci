f56a0e836550cef1ec04e0c47863c1511cd92acf
Nicholas Smith <nicholas.smith@telcoantennas.com.au>
2020-04-29 09:02:18 +1000

    update simple-adblock menu entry

--- applications/luci-app-simple-adblock/luasrc/controller/simple-adblock.lua
+++ applications/luci-app-simple-adblock/luasrc/controller/simple-adblock.lua
@@ -3,5 +3,5 @@ function index()
 	if not nixio.fs.access("/etc/config/simple-adblock") then
 		return
 	end
-	entry({"admin", "services", "simple-adblock"}, cbi("simple-adblock"), _("Simple AdBlock"))
+	entry({"admin", "services", "simple-adblock"}, cbi("simple-adblock"), _("Ad Blocker"))
 end
